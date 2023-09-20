94ff88780fbaebdaf0c43520bb09b54c710d6dfa
sasalla23 <johannesbockhorst2@gmail.com>
2023-09-20 19:47:49 +0200

    Animate keystrokes (Like in Synthesia)

--- src/main.rs
+++ src/main.rs
@@ -523,52 +523,67 @@ const NOTE_FREQUENCIES: [f64;128] = [8.175798915643682,
 use raylib::prelude::*;
 use raylib::core::logging::set_trace_log;
 
+const WHITE_KEY_COUNT: usize = 75;
+const KEY_COUNT: usize = 128;
 
-fn draw_keyboard(d: &mut impl RaylibDraw, bounds: Rectangle, key_map: [Option<Color>;128]) {
-    let white_key_count: usize = 75;
-    let white_key_width = bounds.width / white_key_count as f32;
+fn white_key_rect(index: usize, bounds: Rectangle) -> Rectangle {
+    let key_width = bounds.width / WHITE_KEY_COUNT as f32;
+    Rectangle::new(bounds.x + key_width * index as f32, bounds.y, key_width, bounds.height)
+}
+
+fn black_key_rect(left_white_index: usize, bounds: Rectangle, black_key_width: f32, black_key_height: f32) -> Rectangle {
+    let white_rect = white_key_rect(left_white_index, bounds);
+    Rectangle::new(white_rect.x + white_rect.width - black_key_width, white_rect.y, black_key_width, black_key_height)
+}
+
+fn is_black_key(key: u8) -> bool {
+    match key%12 {
+        1 | 3 | 6 | 8 | 10 => true,
+        _ => false
+    }
+}
+
+fn key_rect(key: u8, bounds: Rectangle) -> Rectangle {
+    let white_key_width = bounds.width / WHITE_KEY_COUNT as f32;
     let black_key_width = white_key_width / 2.0;
     let black_key_height = bounds.height * 0.75;
-    let key_rect = |i|
-        Rectangle::new(bounds.x + white_key_width * i as f32, bounds.y, white_key_width, bounds.height);
-    
-    let get_key_id = |i| {
-        let white_key = i % 7;
-        let mut key_id = (i / 7) * 12;
-        key_id += white_key;
-        if white_key > 0 { key_id += 1 }
-        if white_key > 1 { key_id += 1 }
-        if white_key > 3 { key_id += 1 }
-        if white_key > 4 { key_id += 1 }
-        if white_key > 5 { key_id += 1 }
-        key_id
+
+    let get_white_key_index = |white_key|  {
+        let note = white_key % 12;
+        let mut key_id = white_key / 12 * 7;
+        key_id += note;
+        if note > 1 { key_id -= 1 }
+        if note > 3 { key_id -= 1 }
+        if note > 6 { key_id -= 1 }
+        if note > 8 { key_id -= 1 }
+        if note > 10 { key_id -= 1 }
+        key_id as usize
     };
 
-    for i in 0..white_key_count {
-        let key_id = get_key_id(i);
-        let draw_color = if let Some(color) = key_map[key_id] { color } else { Color::WHITE };
+    if is_black_key(key) {
+        let white_key = key - 1;
+        black_key_rect(get_white_key_index(white_key), bounds, black_key_width, black_key_height)
+    } else {
+        white_key_rect(get_white_key_index(key), bounds)
+    }
+}
 
-        let key_rect = key_rect(i);
-        d.draw_rectangle_rec(key_rect, draw_color);
-        d.draw_rectangle_lines_ex(key_rect, 1, Color::GRAY);
+fn draw_keyboard(d: &mut impl RaylibDraw, bounds: Rectangle, key_map: [Option<Color>;128]) {
+    // Draw white keys
+    for i in 0..KEY_COUNT {
+        if !is_black_key(i as u8) {
+            let draw_color = if let Some(color) = key_map[i] { color } else { Color::WHITE };
+            let rect = key_rect(i as u8, bounds);
+            d.draw_rectangle_rec(rect, draw_color);
+            d.draw_rectangle_lines_ex(rect, 1, Color::GRAY);
+        }
     }
 
-    for i in 0..white_key_count {
-        let key_id = get_key_id(i)+1;
-
-        let white_key = i % 7;
-        let key_rect = key_rect(i);
-        if white_key != 2 && white_key != 6 && i != white_key_count-1 {
-            
-            let black_key_bounds =
-                Rectangle::new(
-                    key_rect.x + key_rect.width - black_key_width * 0.5,
-                    bounds.y,
-                    black_key_width,
-                    black_key_height
-                );
-            let draw_color = if let Some(color) = key_map[key_id] { color } else { Color::BLACK };
-            d.draw_rectangle_rec(black_key_bounds, draw_color);
+    // Draw black keys
+    for i in 0..KEY_COUNT {
+        if is_black_key(i as u8) {
+            let draw_color = if let Some(color) = key_map[i] { color } else { Color::BLACK };
+            d.draw_rectangle_rec(key_rect(i as u8, bounds), draw_color);
         }
     }
 }
@@ -841,6 +856,10 @@ const COLORS: [Color; 8] = [
     Color::SKYBLUE
 ];
 
+fn get_color(channel: u8) -> Color {
+    COLORS[channel as usize % COLORS.len()]
+}
+
 #[derive(Debug,Clone,Copy)]
 struct TrackPlayer {
     event_pointer: usize
@@ -858,6 +877,71 @@ use std::env;
 enum State {
     RENDERING,
     VISUALIZING
+}
+
+struct NoteVisual {
+    channel: u8,
+    key: u8,
+    track: usize,
+    start_time: f64,
+    stop_time: Option<f64>
+}
+
+const TIME_SCALE: f64 = 100.0; // PIXEL / SECOND
+
+impl NoteVisual {
+    fn new(channel: u8, key: u8, track: usize, start_time: f64) -> Self { 
+        Self { channel, key, track, stop_time: None, start_time }
+    }
+
+    fn get_rect(&self, curr_time: f64, keyboard_bounds: Rectangle) -> Rectangle {
+        let keyboard_key_rect = key_rect(self.key, keyboard_bounds);
+        let start_y = match self.stop_time {
+            Some(t) => (curr_time - t) * TIME_SCALE,
+            None => 0.0
+        };
+        let end_y = (curr_time - self.start_time) * TIME_SCALE;
+        Rectangle::new(keyboard_key_rect.x, start_y as f32, keyboard_key_rect.width, (end_y - start_y) as f32)
+    }
+
+    fn render(&self, curr_time: f64, d: &mut impl RaylibDraw, keyboard_bounds: Rectangle) {
+        let color = get_color(self.channel);
+        d.draw_rectangle_rec(self.get_rect(curr_time, keyboard_bounds), color);
+    }
+}
+
+fn update_track_players(track_players: &mut Vec<TrackPlayer>, normed_tracks: &Vec<NormalizedTrack>, elapsed_time: f64, note_visuals: &mut Vec<NoteVisual>) {
+    'outer: for (i, player) in track_players.iter_mut().enumerate() {
+                    
+        if player.event_pointer >= normed_tracks[i].events.len() {
+            continue 'outer;
+        }
+
+        while elapsed_time > normed_tracks[i].events[player.event_pointer].0 {
+            match normed_tracks[i].events[player.event_pointer].1 {
+                NormalizedEvent::KeyOff { key, channel, .. } => {
+                    //key_map[key as usize] = None;
+                    for nv in note_visuals.iter_mut() {
+                        if nv.channel == channel && nv.key == key && nv.stop_time.is_none() {
+                            nv.stop_time = Some(elapsed_time);
+                            break;
+                        }
+                    }
+                },
+                NormalizedEvent::KeyOn { key, channel, .. } => {
+                    //let color = COLORS[channel as usize % COLORS.len()];
+                    //key_map[key as usize] = Some(color);
+                    note_visuals.push(NoteVisual::new(channel, key, i, elapsed_time));
+                },
+            }
+            player.event_pointer += 1;
+            if player.event_pointer >= normed_tracks[i].events.len() {
+                continue 'outer;
+            }
+        
+        }
+        
+    }
 }
 
 fn main() {
@@ -907,40 +991,37 @@ fn main() {
     let mut track_players = vec![TrackPlayer::new(); simult_tracks as usize];
     
     let mut key_map  = [None; 128];
-    
+    let mut note_visuals: Vec<NoteVisual> = Vec::new();
+    let key_board_bounds = Rectangle::new(0.0, WINDOW_HEIGHT as f32 * 7.0/8.0, WINDOW_WIDTH as f32, WINDOW_HEIGHT as f32 / 8.0);
+    let time_offset: f64 = (WINDOW_HEIGHT as f64 - key_board_bounds.height as f64) / TIME_SCALE;
+    println!("TIME OFFSET: {}", time_offset);
     while !rl.window_should_close() {
         match state {
             State::VISUALIZING => {
+                
                 let pi = progress_info.lock().unwrap();
                 let music = music.as_mut().unwrap();
                 let normed_tracks = pi.result.as_ref().unwrap();
+                
 
                 rl_audio.update_music_stream(music);
-                let elapsed_time = rl_audio.get_music_time_played(music) as f64;
-                'outer: for (i, player) in track_players.iter_mut().enumerate() {
-                    
-                    if player.event_pointer >= normed_tracks[i].events.len() {
-                        continue 'outer;
-                    }
-
-                    while elapsed_time > normed_tracks[i].events[player.event_pointer].0 {
-                        match normed_tracks[i].events[player.event_pointer].1 {
-                            NormalizedEvent::KeyOff { key, .. } => {
-                                key_map[key as usize] = None;
-                            },
-                            NormalizedEvent::KeyOn { key, channel, .. } => {
-                                key_map[key as usize] = Some(COLORS[channel as usize % COLORS.len()]);
-                            },
-                        }
-                        player.event_pointer += 1;
-                        if player.event_pointer >= normed_tracks[i].events.len() {
-                            continue 'outer;
-                        }
-                    
-                    }
-                    
-                }
+                let elapsed_time = rl_audio.get_music_time_played(music) as f64 + time_offset;
                 
+                update_track_players(&mut track_players, &normed_tracks, elapsed_time, &mut note_visuals);
+                
+                let initial_note_visual_count = note_visuals.len();
+                for i in 0..initial_note_visual_count {
+                    let index = initial_note_visual_count-(i+1);
+                    let nv = &note_visuals[index];
+                    let nv_rect = nv.get_rect(elapsed_time, key_board_bounds);
+                    if nv_rect.check_collision_recs(&key_rect(nv.key, key_board_bounds)) {
+                        key_map[nv.key as usize] = Some(get_color(nv.channel));
+                    }
+                    if nv_rect.y > key_board_bounds.y {
+                        key_map[nv.key as usize] = None;
+                        note_visuals.remove(index);
+                    }
+                }
 
                 if rl.is_key_pressed(KeyboardKey::KEY_B) {
                     rl.take_screenshot(&thread, "screenshot.png");
@@ -949,8 +1030,11 @@ fn main() {
                 let mut d = rl.begin_drawing(&thread);
                 d.clear_background(Color::BLACK);
                 
-                let key_board_bounds = Rectangle::new(0.0, WINDOW_HEIGHT as f32 * 7.0/8.0, WINDOW_WIDTH as f32, WINDOW_HEIGHT as f32 / 8.0);
+                for key in note_visuals.iter() {
+                    key.render(elapsed_time, &mut d, key_board_bounds)
+                }
                 draw_keyboard(&mut d, key_board_bounds, key_map);
+
             },
             State::RENDERING => {
                 {
@@ -961,7 +1045,7 @@ fn main() {
                         return;
                     }
 
-                    if let Some(_) = &pi.result {
+                    if let Some(normed_tracks) = &pi.result {
                         state = State::VISUALIZING;
                         music = Some({
                             match Music::load_music_stream(&thread, &wav_file_path) {
@@ -973,6 +1057,10 @@ fn main() {
                             }
                         });
                         rl_audio.play_music_stream(&mut music.as_mut().unwrap());
+                        let frames = (time_offset * FPS as f64) as usize;
+                        for frame in 0..frames {
+                            update_track_players(&mut track_players, normed_tracks, frame as f64 / FPS as f64, &mut note_visuals);
+                        }
                     }
                 }
 
--- todo.md
+++ todo.md
@@ -5,17 +5,20 @@
 - [ ] Let user select waveform for instruments
 - [ ] More waveforms
 - [ ] 'Smoother' sounds
+- [ ] Handle more controller messages (e.g. pitch wheel)
 
 ## Midi Parser
 - [ ] Handle more meta events
 - [x] Handle missing signal bytes
 
 ## Visualization
-- [ ] Preview pressed keys
+- [x] Preview pressed keys
 - [ ] VFX
 - [ ] Properly choose colors (Maybe via user input)
 - [x] Show rendering progress before starting the visualization
+- [ ] Make the window resizable
 
 ## Other
 - [x] Better Error handling
 - [ ] Modules
+- [ ] Add option to 'prerender' music and visualization information
